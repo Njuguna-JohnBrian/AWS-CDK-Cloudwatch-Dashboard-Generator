f17165498caf37721b7d23867035c979d9e6e2a9
John Brian <njugunajb96@gmail.com>
2023-01-16 10:34:32 +0300

    create email stack

---
+++ lib/transaction.ts
@@ -0,0 +1,16 @@
+import { Stack } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as cdk from "aws-cdk-lib";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subscriptions from "aws-cdk-lib/aws-sns-subscriptions";
+
+export class EmailStack extends Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const emailTopic = new sns.Topic(this, "Email-Topic");
+    emailTopic.addSubscription(
+      new subscriptions.EmailSubscription("njugunajb96@gmail.com")
+    );
+  }
+}
